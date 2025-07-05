cb5aebc3a44ec706fe4d465a539a7d4662b6878e
Azael6364 <azael.zerpa@correo.unimet.edu.ve>
2025-07-04 20:56:50 -0400

    Creacion de la clase Aminoacido

---
+++ proyect1-edd-bioinformatica/src/proyect1/edd/bioinformatica/Aminoacido.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyect1.edd.bioinformatica;
+
+/**
+ *
+ * @author Azael Zerpa
+ */
+public class Aminoacido {
+    String nombre;
+    String[] tripletas;
+    
+    Aminoacido(String nombre, String[] tripletas) {
+        this.nombre = nombre;
+        this.tripletas = tripletas;
+    }
+}
